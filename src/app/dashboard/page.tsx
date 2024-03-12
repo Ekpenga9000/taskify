ae46c5727bd6ef69b092d4a87163decb014cf9f2
Louis_Ekpenga <ekpenga9000@gmail.com>
2024-03-11 20:44:24 -0600

    Add a sign out button to navbar

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,11 +1,16 @@
 import React from "react";
 import Dashboard from "@/components/Dashboard";
 import InputComponent from "@/components/InputComponent";
+import { auth } from "@/auth";
 
-const DashboardPage = () => {
+const DashboardPage = async() => {
+  const session = await auth(); 
   return (
     <section>
-      <h1 className="text-6xl font-bold text-center">Welcome to Omogbare</h1>
+      <h1 className="text-2xl font-bold text-center">Welcome</h1>
+      <div>
+        {JSON.stringify(session)}
+      </div>
       <p className="text-xl mt-4 text-center">
         Your enchanted realm of task mastery
       </p>
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,17 @@
 import React from "react";
 import Link from "next/link";
+import { signOut } from "@/auth";
+import { LuLogOut } from "react-icons/lu";
 
 const Header = () => {
+  const handleLogout = async () => {
+    "use server";
+    await signOut();
+  };
+
   return (
     <nav className="border-b text-zinc-900 px-10 py-5">
-      <div className="flex justify-between">
+      <div className="flex justify-between items-center">
         <Link href={"/"} className="font-semibold">
           Taskify
         </Link>
@@ -19,6 +26,11 @@ const Header = () => {
             <Link href={"/dashboard/rank"}>Rank</Link>
           </li>
         </ul>
+        <form action={handleLogout}>
+          <button className="text-zinc-100 bg-zinc-700 hover:bg-zinc-900 flex items-center gap-1 px-3 py-1 rounded-md" type="submit">
+          <LuLogOut />  Logout
+          </button>
+        </form>
       </div>
     </nav>
   );
