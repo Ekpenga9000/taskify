f7e9623031f3d9b9dd337b5d977082ed402e96e4
Louis_Ekpenga <ekpenga9000@gmail.com>
2024-03-11 22:42:40 -0600

    Add user role to user table.

--- src/auth.ts
+++ src/auth.ts
@@ -1,17 +1,45 @@
+
 import NextAuth from "next-auth";
+import { UserRole } from '@prisma/client';
 import authConfig from "@/auth.config";
 import { db } from "@/lib/database";
 import { PrismaAdapter } from "@auth/prisma-adapter";
+import { findUserById } from "@/data/user";
 
 export const {
-    handlers: { GET, POST },
-    auth,
-    signIn,
-    signOut,
+  handlers: { GET, POST },
+  auth,
+  signIn,
+  signOut,
 } = NextAuth({
-    adapter: PrismaAdapter(db),
-    session: {strategy: "jwt"},
-    ...authConfig
+    callbacks: {
+         
+    async session({ session, token }) {
+      if (token.sub && session.user) {
+        session.user.id = token.sub;
+      }
+
+      if (token.role && session.user) {
+        session.user.role = token.role as UserRole;
+      }
+
+      return session;
+    },
+    async jwt({ token }) {
+      if (!token.sub) return token;
+
+      const user = await findUserById(token.sub);
+
+      if (!user) return token;
+
+      token.role = user.role;
+
+      return token;
+    },
+  },
+  adapter: PrismaAdapter(db),
+  session: { strategy: "jwt" },
+  ...authConfig,
 });
 
 //Session does not work with Prisma so we would have to use JWT from NextAuth
